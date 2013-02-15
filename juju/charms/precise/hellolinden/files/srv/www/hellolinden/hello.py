1764fbd7f4b2b93953335555438a49a34981c065
rojo <rojo@rojo-pc.lindenlab.com>
2013-02-15 15:10:36 -0800

    added sql-join hook

--- juju/charms/precise/hellolinden/files/srv/www/hellolinden/hello.py
+++ juju/charms/precise/hellolinden/files/srv/www/hellolinden/hello.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config.from_object('hello.cfg')
+db = SQLAlchemy(app)
+
+class Greeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(80), unique=True)
+    
+    def __init__(self, text):
+        self.text = text
+    
+    def __repr__(self):
+        return '<Greeting %r>' % self.username
+    
+db.create_all()
+db.session.add(Greeting('Hello'))
+db.session.add(Greeting('Ahoy'))
+db.session.add(Greeting('G\'day'))
+db.session.add(Greeting('Hey'))
+db.session.add(Greeting('Howdy'))
+db.session.commit()
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    greets = Greeting.query.all()
+    return greets
 
 if __name__ == "__main__":
     app.run()
